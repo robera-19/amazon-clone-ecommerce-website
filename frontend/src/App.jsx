0d73141537d3d4983c7b0d98ad80a80c309d95df
Robera Wakjira <robarobera50@gmail.com>
2026-04-16 13:44:16 +0300

    feat: add home background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import CarouselEffect from "./components/Carousel/Carousel";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <CarouselEffect />
     </div>
   );
 };
