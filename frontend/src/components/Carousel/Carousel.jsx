f1ef9e33a31f9cea5cc7379684073e1aa6b7ee19
Robera Wakjira <robarobera50@gmail.com>
2026-04-16 13:42:07 +0300

    feat: add homepage background image with carousel effect

---
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,24 @@
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./images/data";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import classes from "./Carousel.module.css";
+
+const CarouselEffect = () => {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {img.map((imageItemLink) => {
+          return <img src={imageItemLink} />;
+        })}
+      </Carousel>
+      <div className={classes.hero_image}></div>
+    </div>
+  );
+};
+
+export default CarouselEffect;
