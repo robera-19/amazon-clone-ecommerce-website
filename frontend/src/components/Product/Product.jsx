eb6c4b2a80c547de576f8a4d4a91f8f561666cb3
Robera Wakjira <robarobera50@gmail.com>
2026-04-18 11:03:58 +0300

    feat: implement loading functionality

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -4,22 +4,39 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { ClassNames } from "@emotion/react";
 import classes from "./Product.module.css";
+import Loader from "../../components/Loader/Loader";
 
 function Product() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((res) => {
-      setProducts(res.data);
-    });
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProducts(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
-    <section className={classes.products_container}>
-      {products.map((singleProduct) => {
-        return <ProductCard product={singleProduct} key={singleProduct.id} />;
-      })}
-    </section>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={classes.products_container}>
+          {products.map((singleProduct) => {
+            return (
+              <ProductCard product={singleProduct} key={singleProduct.id} />
+            );
+          })}
+        </section>
+      )}
+    </>
   );
 }
 
