587461d03f0dfddf0ee70b5d1d4e860bcea13c3d
Robera Wakjira <robarobera50@gmail.com>
2026-04-18 09:55:02 +0300

    feat: add products page

---
+++ frontend/src/components/Product/Product.jsx
@@ -0,0 +1,26 @@
+import ProductCard from "./ProductCard";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ClassNames } from "@emotion/react";
+import classes from "./Product.module.css";
+
+function Product() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
+  return (
+    <section className={classes.products_container}>
+      {products.map((singleProduct) => {
+        return <ProductCard product={singleProduct} key={singleProduct.id} />;
+      })}
+    </section>
+  );
+}
+
+export default Product;
