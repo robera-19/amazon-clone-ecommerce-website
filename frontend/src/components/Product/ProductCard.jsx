ecabe4ca55c535c2da6f97d06ba97268aadae8a2
Robera Wakjira <robarobera50@gmail.com>
2026-04-21 22:17:09 +0300

    feat: define add to cart function

--- frontend/src/components/Product/ProductCard.jsx
+++ frontend/src/components/Product/ProductCard.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
 import { Link } from "react-router-dom";
 import Loader from "../Loader/Loader";
+import { DataContext } from "../DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
 
-function ProductCard({ product, flex, RenderDesc }) {
+function ProductCard({ product, flex, RenderDesc, renderAdd }) {
   if (!product) {
     return <Loader />;
   }
 
   const { image, title, id, rating, price, description } = product;
+
+  const [state, dispatch] =useContext(DataContext)
+
+  const addToCart = () => {
+    dispatch({
+      type: Type.ADD_TO_BASKET,
+      item: {
+        image,
+        title,
+        id,
+        rating,
+        price,
+        description,
+      },
+    });
+  }
 
   return (
     <div
@@ -31,7 +49,13 @@ function ProductCard({ product, flex, RenderDesc }) {
           {/*price */}
           <CurrencyFormat amount={price} />
         </div>
-        <button className={classes.button}>Add to Cart</button>
+
+        {
+          renderAdd && <button className={classes.button} onClick={addToCart}>
+          Add to Cart
+        </button>
+        }
+        
       </div>
     </div>
   );
