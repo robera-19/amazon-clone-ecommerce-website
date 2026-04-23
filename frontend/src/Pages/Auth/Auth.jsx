c310707abfd32e88eab09c92c57fcfbb3d3b35f0
Robera Wakjira <robarobera50@gmail.com>
2026-04-23 12:27:54 +0300

    feat: implement firebase authentication with email and password

--- frontend/src/Pages/Auth/Auth.jsx
+++ frontend/src/Pages/Auth/Auth.jsx
@@ -1,11 +1,68 @@
+import { useState, useContext } from "react";
 import classes from "./Signup.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { DataContext } from "../../components/DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
+import { ClipLoader } from "react-spinners";
 
 const Auth = () => {
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+  const [error, setError] = useState(" ");
+  const [loading, setLoading] = useState({
+    signIn: false,
+    signUp: false,
+  });
+
+  const [{ user }, dispatch] = useContext(DataContext);
+  const navigate = useNavigate()
+
+  const authHandler = async (e) => {
+    e.preventDefault();
+
+    if (e.target.name == "signin") {
+      //firebase auth
+      setLoading({ ...loading, signIn: true });
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+          setLoading({ ...loading, signIn: false });
+          navigate("/")
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading({ ...loading, signIn: false });
+        });
+    } else {
+      setLoading({ ...loading, signUp: true });
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          dispatch({
+            type: Type.SET_USER,
+            user: userInfo.user,
+          });
+          setLoading({ ...loading, signUp: false });
+          navigate("/");
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading({ ...loading, signUp: false });
+        });
+    }
+  };
+
   return (
     <section className={classes.login}>
       {/*logo */}
-      <Link>
+      <Link to="/">
         <img
           src="https://i.pinimg.com/736x/09/a1/1b/09a11bf7d3b75dd36cd3a2089828c7f3.jpg"
           alt="amazon logo"
@@ -19,15 +76,32 @@ const Auth = () => {
         <form action="">
           <div>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
           </div>
 
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+            />
           </div>
 
-          <button className={classes.login__signInButton}>Sign In</button>
+          <button
+            type="submit"
+            name="signin"
+            onClick={authHandler}
+            className={classes.login__signInButton}
+          >
+            {loading.signIn ? <ClipLoader color="#000" size={15} /> : "Sign In"}
+          </button>
         </form>
 
         {/*terms and service agreement */}
@@ -38,7 +112,22 @@ const Auth = () => {
         </p>
 
         {/*create account button */}
-        <button className={classes.login__registerButton}>Create your amazon account</button>
+        <button
+          type="submit"
+          name="signup"
+          onClick={authHandler}
+          className={classes.login__registerButton}
+        >
+          {loading.signUp ? (
+            <ClipLoader color="#000" size={15} />
+          ) : (
+            "Create your amazon account"
+          )}
+        </button>
+
+        {error && (
+          <small style={{ paddingTop: "10px", color: "red" }}>{error}</small>
+        )}
       </div>
     </section>
   );
